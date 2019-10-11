af2ab067289c9b7387813f305295a1d1b98e2893
kajal <kajal.bayas@techprimelab.com>
2019-10-11 20:07:35 +0530

    Resloved CROS error

--- TollApp_Backend/Controllers/TollPlazasController.cs
+++ TollApp_Backend/Controllers/TollPlazasController.cs
@@ -25,92 +25,12 @@ namespace TollApp_Backend.Controllers
         }
 
         // GET: api/TollPlazas/5
-        // [ResponseType(typeof(TollPlaza))]
-        public IQueryable GetTollPlaza(int id)
+      public IQueryable GetTollPlaza(int id)
         {
             var ExitLocation = db.TollPlazas.Where(e => e.RouteId == id).Select(l=> l.Location);
             return ExitLocation;
    
         }
 
-        // PUT: api/TollPlazas/5
-        //[ResponseType(typeof(void))]
-        //public IHttpActionResult PutTollPlaza(int id, TollPlaza tollPlaza)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    if (id != tollPlaza.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    db.Entry(tollPlaza).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        db.SaveChanges();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!TollPlazaExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return StatusCode(HttpStatusCode.NoContent);
-        //}
-
-        //// POST: api/TollPlazas
-        //[ResponseType(typeof(TollPlaza))]
-        //public IHttpActionResult PostTollPlaza(TollPlaza tollPlaza)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    db.TollPlazas.Add(tollPlaza);
-        //    db.SaveChanges();
-
-        //    return CreatedAtRoute("DefaultApi", new { id = tollPlaza.Id }, tollPlaza);
-        //}
-
-        //// DELETE: api/TollPlazas/5
-        //[ResponseType(typeof(TollPlaza))]
-        //public IHttpActionResult DeleteTollPlaza(int id)
-        //{
-        //    TollPlaza tollPlaza = db.TollPlazas.Find(id);
-        //    if (tollPlaza == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    db.TollPlazas.Remove(tollPlaza);
-        //    db.SaveChanges();
-
-        //    return Ok(tollPlaza);
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
-
-        //private bool TollPlazaExists(int id)
-        //{
-        //    return db.TollPlazas.Count(e => e.Id == id) > 0;
-        //}
     }
 }
